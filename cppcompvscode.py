e5bf65ea7dfd2cf799a919c0ae89003baab75a5d
nomfodm <nomfodm@gmail.com>
2024-04-12 16:31:17 +0300

    Initial commit

---
+++ cppcompvscode.py
@@ -0,0 +1,127 @@
+import json
+import os
+import subprocess
+import sys
+import urllib.request
+
+from rich.progress import Progress, TextColumn, BarColumn, DownloadColumn, TransferSpeedColumn, TimeRemainingColumn
+
+config = {"extensions": ["ms-vscode.cpptools-extension-pack", "formulahendry.code-runner",
+                         "EliverLara.andromeda"],
+          "settings": {
+              "linux": {
+                  "code-runner.runInTerminal": True,
+                  "code-runner.saveAllFilesBeforeRun": True,
+                  "code-runner.saveFileBeforeRun": True,
+                  "workbench.colorTheme": "Andromeda Bordered",
+                  "C_Cpp.default.compilerPath": "/usr/bin/g++",
+              },
+              "windows": {
+                  "code-runner.runInTerminal": True,
+                  "code-runner.saveAllFilesBeforeRun": True,
+                  "code-runner.saveFileBeforeRun": True,
+                  "workbench.colorTheme": "Andromeda Bordered",
+                  "code-runner.executorMap": {
+                      "c": f"cd $dir && {os.path.expanduser('~')}\\mingw64\\bin\\gcc $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+                      "cpp": f"cd $dir && {os.path.expanduser('~')}\\mingw64\\bin\\g++ $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+                  },
+                  "C_Cpp.default.compilerPath": f"{os.path.expanduser('~')}\\mingw64\\bin\\g++",
+              }
+          },
+          "linux": [["sudo", "apt", "update"], ["sudo", "apt", "install", "gcc"]],
+          "windows": {
+              "mingwUrl": "https://github.com/niXman/mingw-builds-binaries/releases/download/13.2.0-rt_v11-rev1"
+                          "/x86_64-13.2.0-release-win32-seh-msvcrt-rt_v11-rev1.7z"
+          },
+          "settingsJsonLocation": {
+              "windows": "%APPDATA%\\Code\\User\\settings.json",
+              "linux": "$HOME/.config/Code/User/settings.json"
+          },
+          "codeCommand": {
+              "windows": "%LOCALAPPDATA%\\Programs\\Microsoft VS Code\\bin\\code",
+              "linux": "/usr/share/code/bin/code"
+          },
+          }
+
+
+class Request:
+    @staticmethod
+    def download(url: str, filename: str) -> None:
+        with Progress(
+                TextColumn("[bold blue]{task.fields[filename]}", justify="right"),
+                BarColumn(bar_width=None),
+                "[progress.percentage]{task.percentage:>3.1f}%",
+                "•",
+                DownloadColumn(),
+                "•",
+                TransferSpeedColumn(),
+                "•",
+                TimeRemainingColumn(),
+        ) as progress:
+            downloading = progress.add_task("download", filename=filename)
+            urllib.request.urlretrieve(url, filename,
+                                       lambda block, size, total: progress.update(downloading, total=total,
+                                                                                  completed=block * size))
+
+
+def main():
+    from rich import print
+
+    print("\nInstalling necessary stuff...")
+    if args[0] == "linux":
+        for command in config[args[0]]:
+            subprocess.call(command)
+    elif args[0] == "windows":
+        import py7zr
+        Request.download(config[args[0]]["mingwUrl"], "ia\\mingw64.7z")
+        print("Extracting...")
+        with py7zr.SevenZipFile('ia\\mingw64.7z', mode='r') as z:
+            z.extractall(os.path.expanduser("~") + "\\")
+        os.remove("ia\\mingw64.7z")
+
+    extensions = config["extensions"]
+    for extension in extensions:
+        subprocess.call([config["codeCommand"][args[0]], "--install-extension", extension], shell=True)
+
+    print("\nSetting settings.json...")
+    if not os.path.isfile(config["settingsJsonLocation"][args[0]]):
+        with open(config["settingsJsonLocation"][args[0]], "x") as file:
+            file.write("{}")
+
+    with open(config["settingsJsonLocation"][args[0]], "r") as file:
+        settingsFile = json.load(file)
+
+    for key in config["settings"][args[0]]:
+        print(key, ": ", config["settings"][args[0]][key], sep="")
+        settingsFile[key] = config["settings"][args[0]][key]
+
+    with open(config["settingsJsonLocation"][args[0]], "w") as file:
+        file.write(json.dumps(settingsFile))
+
+    print("\n[green]Configuring completed.")
+
+
+def checkArgs():
+    if args[0] not in ["windows", "linux"]:
+        print("Wrong system type specified")
+        exit(0)
+
+
+if __name__ == '__main__':
+    if not sys.argv[1:]:
+        print("This script is destined for Installation Helper")
+        exit(0)
+
+    args = sys.argv[1:]
+    checkArgs()
+
+    if args[0].startswith("windows"):
+        config["settingsJsonLocation"][args[0]] = config["settingsJsonLocation"][args[0]].replace("%APPDATA%",
+                                                                                                  os.getenv("APPDATA"))
+        config["codeCommand"][args[0]] = config["codeCommand"][args[0]].replace("%LOCALAPPDATA%",
+                                                                                os.getenv("LOCALAPPDATA"))
+    elif args[0].startswith("linux"):
+        config["settingsJsonLocation"][args[0]] = config["settingsJsonLocation"][args[0]].replace("$HOME",
+                                                                                                  os.getenv("HOME"))
+
+    main()
---
+++ main.py
@@ -0,0 +1,253 @@
+import os
+import shutil
+import subprocess
+import sys
+import typing
+
+import urllib.request
+import urllib.error
+import json
+
+import urllib.request
+
+from rich.progress import Progress, TextColumn, BarColumn, DownloadColumn, TransferSpeedColumn, \
+    TimeRemainingColumn
+
+from rich import print
+from rich.prompt import Prompt
+
+CONFIG = {
+    "version": "1.0",
+    "language": "en",
+    "messages": {
+        "systemNotSupported": "[red]Your OS is not supported.",
+        "errorReceivingLangConf": "[red]Unable to get language config from server, "
+                                  "check your internet connection, shutting down.",
+        "errorReceivingAppsConf": "[red]Unable to get app config from server, "
+                                  "check your internet connection, shutting down.",
+        "pressEnterToContinue": "Press 'enter' key to continue...",
+        "keyboardInterrupt": "[green]\n\nProcess interrupted by user.",
+        "exceptionCaught": "[red]\n\nException caught: [blue]%s",
+        "reportItToGithubIssues": "[red]Please report it to github issues, https://github.com/nomfodm/ia/issues",
+
+        "welcome": "[green]Welcome to Installation Assistant, [red]v%s\n[white]This program will "
+                   "install any application you want from the list with chosen configuration",
+        "chooseAppYouWantToInstall": "Choose app you want to install",
+
+        "noConfigurationNeeded": "No configuration needed",
+        "chooseConfigForApp": "Now, I suggest you choosing configuration you need.",
+
+        "youChosenAConfiguration": "[green]OK, you chose [blue]%s [green]configuration.",
+        "youDontNeedAConfiguration": "[green]OK, no configuration, I just will install [blue]%s [green]for you.",
+
+        "downloadingSetupFile": "[green]\nDownloading setup file...",
+        "noSetupFile": "[green]\nNo setup file needed for this application",
+        "installing": "[green]\nInstalling...",
+        "installedSuccessfully": "[blue]\n%s [green]installed successfully!",
+
+        "nowIWillApplyConfiguration": "[green]\nNow, I will apply [blue]%s [green]configuration for [bold blue]%s[green].",
+        "errorOccurredWhiteConfiguring": "[red]\nError occurred while configuring [blue]%s [green]configuration for [bold blue]%s[green].",
+        "configuringCompleted": "[green]\nConfiguration completed.",
+
+        "installationCompleted": "[green]\nInstallation completed.",
+
+        "thanksForUsingIA": "[bold green]\nThanks for using Installation Assistant!"
+    }
+}
+
+DEV = False
+
+
+class Helper:
+    @staticmethod
+    def getSystemName() -> str:
+        if sys.platform.startswith("win32"):
+            return "windows"
+        elif sys.platform.startswith("linux"):
+            return "linux"
+
+        return ""
+
+    @staticmethod
+    def cleanUp() -> None:
+        shutil.rmtree("ia", ignore_errors=True)
+
+    @staticmethod
+    def clearConsole() -> None:
+        subprocess.call('cls' if os.name == 'nt' else 'clear', shell=True)
+
+    @staticmethod
+    def pauseCommand() -> None:
+        input(CONFIG["messages"]["pressEnterToContinue"])
+
+    @staticmethod
+    def addFolderToFilename(filename: str) -> str:
+        return os.path.join("ia", filename)
+
+    @staticmethod
+    def die(message: str = "") -> None:
+        print("\n")
+        print(message)
+        Helper.pauseCommand()
+        exit(0)
+
+    @staticmethod
+    def createPrintableAppListStringAndChooseList() -> typing.Tuple[str, list[str]]:
+        appListString = ""
+        chooseList = []
+        for i, app in enumerate(CONFIG["apps"]):
+            appListString += f"\t{i + 1}. {app['fullAppName']}\n"
+            chooseList.append(str(i + 1))
+
+        return appListString, chooseList
+
+    @staticmethod
+    def createPrintableConfigurationsListAndChooseList(app: typing.Dict) -> typing.Tuple[str, list[str]]:
+        configurationsListString = f"\t0. {CONFIG['messages']['noConfigurationNeeded']}\n"
+        chooseList = ['0']
+        for i, configuration in enumerate(app["configurations"]):
+            configurationsListString += f"\t{i + 1}. {configuration['fullConfigName']}\n"
+            chooseList.append(str(i + 1))
+        return configurationsListString, chooseList
+
+    @staticmethod
+    def installApp(app: typing.Dict) -> None:
+        system = Helper.getSystemName()
+
+        setupSection = app["setup"][system]
+        if setupSection["setupFilename"] != "noSetupFile":
+            print(CONFIG["messages"]["downloadingSetupFile"])
+            Request.download(setupSection["setupUrl"], Helper.addFolderToFilename(setupSection["setupFilename"]))
+        else:
+            print(CONFIG["messages"]["noSetupFile"])
+
+        print(CONFIG["messages"]["installing"])
+        if not DEV:
+            subprocess.call(setupSection["setupCommand"])
+
+        Helper.clearConsole()
+        print(CONFIG["messages"]["installedSuccessfully"] % app["shortAppName"])
+
+    @staticmethod
+    def configureApp(app: typing.Dict, configuration: typing.Dict) -> None:
+        print()
+        print(
+            CONFIG["messages"]["nowIWillApplyConfiguration"] % (configuration["shortConfigName"], app["shortAppName"]))
+        scriptFilename = Helper.addFolderToFilename(configuration["configureScriptFilename"])
+        Request.download(configuration["configureScriptUrl"], scriptFilename)
+
+        status = subprocess.call([sys.executable, scriptFilename, Helper.getSystemName()])
+        if status != 0:
+            print(CONFIG["messages"]["errorOccurredWhiteConfiguring"])
+            print(CONFIG['messages']["reportItToGithubIssues"])
+
+            Helper.cleanUp()
+            Helper.die()
+
+        print(CONFIG["messages"]["configuringCompleted"])
+
+
+class Request:
+    @staticmethod
+    def getAppsConfig() -> None:
+
+        try:
+            with urllib.request.urlopen("https://infinitymc.ru/ih/info.json") as req:
+                CONFIG["apps"] = json.loads(req.read().decode("utf-8"))["apps"]
+        except urllib.error.HTTPError:
+            Helper.die(CONFIG['messages']["errorReceivingAppsConf"])
+        except urllib.error.URLError:
+            Helper.die(CONFIG['messages']["errorReceivingAppsConf"])
+
+    @staticmethod
+    def getLanguageConfig() -> None:
+        if CONFIG["language"] == "en":
+            return
+
+        try:
+            with urllib.request.urlopen(f"https://infinitymc.ru/ih/{CONFIG['language']}.json") as req:
+                CONFIG["messages"] = json.loads(req.read().decode("utf-8"))
+        except urllib.error.HTTPError:
+            Helper.die(CONFIG['messages']["errorReceivingLangConf"])
+        except urllib.error.URLError:
+            Helper.die(CONFIG['messages']["errorReceivingLangConf"])
+
+    @staticmethod
+    def download(url: str, filename: str) -> None:
+        with Progress(
+                TextColumn("[bold blue]{task.fields[filename]}", justify="right"),
+                BarColumn(bar_width=None),
+                "[progress.percentage]{task.percentage:>3.1f}%",
+                "•",
+                DownloadColumn(),
+                "•",
+                TransferSpeedColumn(),
+                "•",
+                TimeRemainingColumn(),
+        ) as progress:
+            downloading = progress.add_task("download", filename=filename)
+            urllib.request.urlretrieve(url, filename,
+                                       lambda block, size, total: progress.update(downloading, total=total,
+                                                                                  completed=block * size))
+
+
+def main() -> None:
+    os.makedirs("ia", exist_ok=True)
+
+    system = Helper.getSystemName()
+    if not system:
+        Helper.die(CONFIG["messages"]["systemNotSupported"])
+
+    if not DEV:
+        language = Prompt.ask("Choose your language/Выберите язык", choices=["en", "ru"])
+        CONFIG["language"] = language.lower()
+
+    Request.getLanguageConfig()
+    Request.getAppsConfig()
+
+    Helper.clearConsole()
+    print(CONFIG["messages"]["welcome"] % CONFIG["version"])
+
+    appListString, chooseList = Helper.createPrintableAppListStringAndChooseList()
+    print(appListString)
+    chosenAppId = int(Prompt.ask(CONFIG["messages"]["chooseAppYouWantToInstall"], choices=chooseList))
+
+    app = CONFIG["apps"][chosenAppId - 1]
+
+    Helper.clearConsole()
+    print(CONFIG["messages"]["chooseConfigForApp"])
+    configListString, configChooseList = Helper.createPrintableConfigurationsListAndChooseList(app)
+    print(configListString)
+    chosenConfigId = int(Prompt.ask(choices=configChooseList))
+
+    Helper.clearConsole()
+    if not chosenConfigId:
+        print(CONFIG["messages"]["youDontNeedAConfiguration"] % app["shortAppName"])
+    else:
+        print(CONFIG["messages"]["youChosenAConfiguration"] % app["configurations"][chosenConfigId - 1][
+            "shortConfigName"])
+
+    Helper.installApp(app)
+    if chosenConfigId:
+        Helper.configureApp(app, app["configurations"][chosenConfigId - 1])
+
+    print()
+    print(CONFIG["messages"]["installationCompleted"])
+    print(CONFIG["messages"]["thanksForUsingIA"])
+
+    Helper.cleanUp()
+    Helper.die()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        Helper.cleanUp()
+        Helper.die(CONFIG['messages']["keyboardInterrupt"])
+    except Exception as e:
+        print(CONFIG['messages']["exceptionCaught"] % e)
+        print(CONFIG['messages']["reportItToGithubIssues"])
+
+        Helper.cleanUp()
+        Helper.die()
