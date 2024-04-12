e5c0712550ab856817c93fecdc66c04f219dbbd9
nomfodm <nomfodm@gmail.com>
2024-04-12 19:27:39 +0300

    move some files to new folders, and simple launcher

---
+++ launcher.py
@@ -0,0 +1,34 @@
+import os.path
+import subprocess
+import sys
+import urllib.request
+
+
+def cleanUp() -> None:
+    if os.path.isfile("main.py"):
+        os.remove("main.py")
+
+
+def main() -> None:
+    print("Installing pypi dependencies...")
+    subprocess.call(
+        [sys.executable, "-m", "pip", "install", "rich", "py7zr" if sys.platform.startswith("win32") else ""])
+
+    print("\n\nDownloading main.py...")
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/nomfodm/ia/master/main.py", "main.py")
+
+    print("\n\nStarting...\n")
+    subprocess.call([sys.executable, "main.py"])
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        cleanUp()
+        exit(0)
+    except Exception as e:
+        print("\n\nUnexpected error:", e)
+        print("Report this exception to https://github.com/nomfodm/ia/issues")
+        cleanUp()
+        exit(0)
